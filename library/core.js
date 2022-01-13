f87a165b8079ce29cda5199e79d3eb6b3b9c1377
luong228 <90311663+luong228@users.noreply.github.com>
2022-01-13 12:41:13 +0700

    Update core.js

--- library/core.js
+++ library/core.js
@@ -3,7 +3,7 @@ export default function html([first, ...strings], ...values) {
         (acc, cur) => acc.concat(cur, strings.shift()),
         [first]
         )
-        .filter(x => x && x !== true || x == 0)
+        .filter(x => x && x !== true || x === 0)
         .join('')
 }
 
