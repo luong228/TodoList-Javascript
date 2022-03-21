4e39d050441b59b2265428dc1f1a2b4b09621f10
luong228 <90311663+luong228@users.noreply.github.com>
2022-03-21 13:01:54 +0700

    Update TodoItem.js

--- component/TodoItem.js
+++ component/TodoItem.js
@@ -5,11 +5,11 @@ function TodoItem({ todo, index, editIndex }) {
         <li class="${todo.completed && 'completed'} ${editIndex === index && 'editing'}">
             <div class="view">
                     <input 
-                    class="toggle" 
-                    type="checkbox" 
-                    ${todo.completed && 'checked'}
-                    onchange="dispatch('toggle', ${index})"
-                >
+                        class="toggle" 
+                        type="checkbox" 
+                        ${todo.completed && 'checked'}
+                        onchange="dispatch('toggle', ${index})"
+                    >
                 <label ondblclick="dispatch('startEditing', ${index})">${todo.title}</label>
                 <button class="destroy" onclick="dispatch('destroy', ${index})"></button>
             </div>
